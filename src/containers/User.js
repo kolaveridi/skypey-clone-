a5a0943894a8e9309847711df545700c60570e46
satya_jha <satyajeetjha06@gmail.com>
2018-07-25 16:47:44 +0530

    user rendering

--- src/containers/User.js
+++ src/containers/User.js
@@ -9,15 +9,27 @@ const User = ({ user }) => {
  console.log()
  console.log('user inside User is',_.values(user));
  let abc=_.values(user);
+ console.log(abc);
+ const show=abc.map((val,index)=> {
+  return(
+      <div>
+      <img src={val.profile_pic} alt={val.name} className="User__pic"/>
+      <p className="User__details-name">{val.name}</p>
+      <p className="User__details-status">{val.status}</p>
+      </div>
+  );
+
+});
+
  return (
- <div className="User">
- <img src={abc[0].profile_pic} alt={abc[0].name} className="User__pic" />
- <div className="User__details">
- <p className="User__details-name">{abc[0].name}</p>
- <p className="User__details-status">{abc[0].status}</p>
- </div>
+ <div>
+ {show}
  </div>
  );
 };
-
+// <img src={abc[1].profile_pic} alt={abc[1].name} className="User__pic" />
+// <div className="User__details">
+// <p className="User__details-name">{abc[1].name}</p>
+// <p className="User__details-status">{abc[1].status}</p>
+// </div>
 export default User;
